b5eb9ad6b81e43fd71391dacffffced9f94c3e9e
Yazid Al Ghozali <yazid.al2418@gmail.com>
2025-07-09 21:46:05 +0700

    foldering based on module features

--- extension/src/App.tsx
+++ extension/src/App.tsx
@@ -1,10 +1,10 @@
-import Home from "./pages/Home"
+import Home from "./features/home/Home"
 import { ThemeProvider } from "./components/ui/theme-provider"
-import AnalyzeAddress from "./pages/AnalyzeAdress"
-import AnalyzeSmartContract from "./pages/AnalyzeSmartContract"
+import AnalyzeAddress from "./features/analyze_address/AnalyzeAdress"
+import AnalyzeSmartContract from "./features/analyze_smartcontract/AnalyzeSmartContract"
 import { Route, Routes } from "react-router-dom"
 import { ROUTES } from "./constants/routes"
-import AnalyzeAdressResult from "./pages/AnalyzeAddressResult"
+import AnalyzeAdressResult from "./features/analyze_address/AnalyzeAddressResult"
 
 function App() {
 
