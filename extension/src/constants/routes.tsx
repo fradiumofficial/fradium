a91a2d97a7ffc5108a05eaa26a0bfbcc863f8e04
Yazid Al Ghozali <yazid.al2418@gmail.com>
2025-07-09 22:34:34 +0700

    add new route to smart contract result

--- extension/src/constants/routes.tsx
+++ extension/src/constants/routes.tsx
@@ -3,4 +3,5 @@ export const ROUTES = {
   ANALYZE_ADDRESS: '/analyze-address',
   ANALYZE_SMART_CONTRACT: '/analyze-smart-contract',
   ANALYZE_ADDRESS_RESULT: '/analyze-address-result',
+  ANALYZE_SMART_CONTRACT_RESULT: '/analyze-smart-contract-result',
 }
