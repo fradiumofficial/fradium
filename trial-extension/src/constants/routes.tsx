c5de09a96e3f19f1d6a9d9d23c2fed206f8f6e74
Yazid Al Ghozali <yazid.al2418@gmail.com>
2025-07-09 21:31:01 +0700

    fix: update

--- trial-extension/src/constants/routes.tsx
+++
@@ -1,6 +0,0 @@
-export const ROUTES = {
-  HOME: '/',
-  ANALYZE_ADDRESS: '/analyze-address',
-  ANALYZE_SMART_CONTRACT: '/analyze-smart-contract',
-  ANALYZE_ADDRESS_RESULT: '/analyze-address-result',
-}
