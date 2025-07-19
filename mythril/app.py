9111ce349ecc872a69b2c0aa7bc26a78376e08a5
Gavin Alinski <fradiumofficial@gmail.com>
2025-07-19 14:17:24 +0700

    fix: add max execution timeout on mythril

--- mythril/app.py
+++ mythril/app.py
@@ -15,7 +15,7 @@ def analyze_contract():
         return jsonify({"error": "Contract address is required"}), 400
 
     try:
-        command = ["myth", "analyze", "-a", address, "-o", "json", "-t", "10"]
+        command = ["myth", "analyze", "-a", address, "-o", "json", "-t", "3", "--execution-timeout", "20"]
         env = os.environ.copy()
         env["INFURA_ID"] = INFURA_ID
 
@@ -47,4 +47,4 @@ def analyze_contract():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5001)
