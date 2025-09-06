8253e18d0261ab502f5a543eae16b17ee616bb73
wildanniam <wildanniam4@gmail.com>
2025-09-06 23:26:32 +0700

    cek

--- src/frontend/src/pages/home/Home.jsx
+++ src/frontend/src/pages/home/Home.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import ButtonGreen from "@/core/components/ButtonGreen.jsx";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "@/core/providers/AuthProvider.jsx";
 
 const Home = () => {
     const BACKGROUND_URL = "https://cdn.jsdelivr.net/gh/fradiumofficial/fradium-asset@main/backgrounds/background-1.webp";
